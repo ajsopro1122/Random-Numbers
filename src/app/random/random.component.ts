fca3dde7b69c0af3d14eda1c5cdff6a0dacd9bcc
ajsopro1122 <71369722+ajsopro1122@users.noreply.github.com>
2021-10-06 20:50:23 +0800

    updated

--- src/app/random/random.component.ts
+++ src/app/random/random.component.ts
@@ -8,17 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class RandomComponent{
 
   constructor() { 
-    this.generate()
+    for (var x=0; x<6; x++) {
+      this.rndNumber.push(Math.floor(Math.random()*46+1))
+    }
   }
 
   rndNumber:any = []
   selected:any
 
-  generate(){
-    for (var x=0; x<6; x++){
-      this.rndNumber.push(Math.floor(Math.random()*46)+1)
-    }
-  }
+
 
   buttonSelect(num: number){
     this.selected = num
